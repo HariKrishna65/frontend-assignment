e68ec60bc4a74e9faa3f4464166a7d35a59e4c74
HariKrishna65 <thondapu.harikrishna999@gmail.com>
2026-03-21 16:01:05 +0530

    feat: add product data

---
+++ mini_app/product_inventory_dashboard/script.js
@@ -0,0 +1,5 @@
+let products = [
+    { id: 1, name: "Laptop", price: 55000, stock: 5, category: "electronics" },
+    { id: 2, name: "Shirt", price: 1200, stock: 3, category: "clothing" },
+    { id: 3, name: "Book", price: 500, stock: 10, category: "books" }
+];
